106da843d0ded6f0e968f02b17a0ddee596a5b9a
kiran <kiran404.kbk@gmail.com>
2020-04-23 20:38:31 +0530

    changes and counter

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -45,3 +45,49 @@ $(document).ready(function(){
         wrapperMenu.classList.toggle('open');
     })
 });
+$(window).on('load',function(){
+    var containerHeight = $(".pracCont").height();
+    var CounterHight = containerHeight-100+"px";
+    var halfpx = containerHeight / 2;
+    var pixel = halfpx + "px";
+    var contPixel = halfpx/1.3;
+    var pixel2 = contPixel+"px";
+    $('.counter').css('transform', 'translate(0px, ' + pixel + ')');
+    $('.Counter-content').css('transform', 'translate(0px, ' + pixel + ')');
+    $('.Counter-content').css('height', CounterHight);
+    // $('.Counter-content').css('transform', 'translate(0px, 200px)');
+    // $(".counter1").css({ 'transform': 'translateY(' + containerHeight + ')' });
+
+
+
+
+    $.fn.jQuerySimpleCounter = function (options) {
+        var settings = $.extend({
+            start: 0,
+            end: 100,
+            easing: 'swing',
+            duration: 400,
+            complete: ''
+        }, options);
+
+        var thisElement = $(this);
+
+        $({ count: settings.start }).animate({ count: settings.end }, {
+            duration: settings.duration,
+            easing: settings.easing,
+            step: function () {
+                var mathCount = Math.ceil(this.count);
+                thisElement.text(mathCount);
+            },
+            complete: settings.complete
+        });
+    };
+
+
+    $('#number1').jQuerySimpleCounter({ end: 12, duration: 3000 });
+    $('#number2').jQuerySimpleCounter({ end: 55, duration: 3000 });
+    $('#number3').jQuerySimpleCounter({ end: 359, duration: 2000 });
+    $('#number4').jQuerySimpleCounter({ end: 246, duration: 2500 });
+
+
+});
